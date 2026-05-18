403d6bba629c32290f05ecf41ee2f983336890d7
Benedikt <bpalme68@gmail.com>
2026-05-18 11:25:51 +0200

    changed header

--- wp-bepalmet-custom.php
+++ wp-bepalmet-custom.php
@@ -21,7 +21,7 @@
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       bepalmet_custom
+ * Text Domain:       wp-bepalmet-custom
  * Domain Path:       /languages
  */
 
